3fb268b01144f2fff8fa167a222e079989b8b4c7
rustambekovich <muhammadqodir5050@gmail.com>
2023-12-04 21:47:42 +0500

    Auth add Register

--- src/WorldOfBooks.Service/Service/Auth/AuthService.cs
+++ src/WorldOfBooks.Service/Service/Auth/AuthService.cs
@@ -1,4 +1,9 @@
-﻿using WorldOfBooks.Persistence.Dtos.Auth;
+﻿using AutoMapper;
+using Microsoft.Extensions.Caching.Memory;
+using WorldOfBooks.Application.Exceptions.Users;
+using WorldOfBooks.DataAccess.IRepositories;
+using WorldOfBooks.Domain.Entities.Users;
+using WorldOfBooks.Persistence.Dtos.Auth;
 using WorldOfBooks.Persistence.Dtos.User;
 using WorldOfBooks.Persistence.ViewModels.Auth;
 using WorldOfBooks.Service.Interfaces.Auth;
@@ -7,14 +12,57 @@ namespace WorldOfBooks.Service.Service.Auth;
 
 public class AuthService : IAuthService
 {
+    private const int CACHED_FOR_MINUTS_REGISTER = 60;
+    private const int CACHED_FOR_MINUTS_VEFICATION = 5;
+    private const int VERIFICATION_MAXIMUM_ATTEMPTS = 3;
+
+    private const string REGISTER_CACHE_KEY = "register_";
+    private const string VERIFY_REGISTER_CACHE_KEY = "verify_register_";
+    private const string Reset_CACHE_KEY = "reset_";
+    private IMapper _mapper;
+    private IRepository<User> _userRepository;
+    private IMemoryCache _memoryCache;
+
+    public AuthService(
+        IMapper mapper,
+        IRepository<User> userRepository,
+        IMemoryCache memory)
+    {
+        _mapper = mapper;
+        _userRepository = userRepository;
+        _memoryCache = memory;
+    }
+
     public Task<LoginResult> LoginAsync(UserLoginDto dto)
     {
         throw new NotImplementedException();
     }
 
-    public Task<RegisterResult> RegisterAsync(UserCreateDto dto)
+    public async Task<RegisterResult> RegisterAsync(UserCreateDto dto)
     {
-        throw new NotImplementedException();
+        var dbResult = await _userRepository.SelectAsync(user => user.Phone.Equals(dto.Phone));
+
+        if (dbResult is not null)
+            throw new UserAlreadyExistsException();
+
+        if (_memoryCache.TryGetValue(REGISTER_CACHE_KEY + dto.Phone, out UserCreateDto userDto))
+        {
+            userDto.Phone = userDto.Phone;
+            _memoryCache.Remove(dto.Phone);
+        }
+        else
+        {
+            _memoryCache.Set(REGISTER_CACHE_KEY + dto.Phone, dto, TimeSpan.FromMinutes
+                (CACHED_FOR_MINUTS_REGISTER));
+        }
+
+        RegisterResult register = new RegisterResult
+        {
+            Result = true,
+            CacheRegisterMinutes = CACHED_FOR_MINUTS_REGISTER
+        };
+
+        return register;
     }
 
     public Task<SendCodeResult> SendCodeForRegister(SendCodeDto dto)
