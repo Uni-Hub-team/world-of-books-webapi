93d657e6f36d4e0c895e4e2dc4900fa044cf28e5
rustambekovich <muhammadqodir5050@gmail.com>
2023-12-11 11:38:53 +0500

    Create Author Interface

---
+++ src/WorldOfBooks.Service/Interfaces/Authors/IAuthorService.cs
@@ -0,0 +1,18 @@
+﻿using WorldOfBooks.Domain.Configurations;
+using WorldOfBooks.Domain.Enums;
+using WorldOfBooks.Persistence.Dtos.Authors;
+using WorldOfBooks.Persistence.Dtos.RoleDto;
+using WorldOfBooks.Persistence.Dtos.User;
+using WorldOfBooks.Persistence.ViewModels.Authors;
+using WorldOfBooks.Persistence.ViewModels.User;
+
+namespace WorldOfBooks.Service.Interfaces.Authors;
+
+public interface IAuthorService
+{
+    Task<AuthorResult> CreateAsync(AuthorCreateDto dto);
+    Task<AuthorResult> UpdateAsync(long id, AuthorUpdateDto dto);
+    Task<bool> DeleteAsync(long id);
+    Task<AuthorResult> GetByIdAsync(long id);
+    Task<IEnumerable<AuthorResult>> GetAllAsync();
+}
