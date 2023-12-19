59681cfd9ee2418d81a2fbcf25b198532a8c15de
Nurullo <ghostone234qwer@gmail.com>
2023-12-19 14:43:28 +0500

    Migration extension in database

---
+++ src/WorldOfBooks.WebApi/Extensions/MigrationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldOfBooks.DataAccess.Contexts;
+
+namespace WorldOfBooks.WebApi.Extensions;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        {
+            try
+            {
+                var dbContext = scope.ServiceProvider.GetService<WorldOfBooksDbContext>();
+                dbContext.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Migration failed: {ex.Message}");
+            }
+        }
+    }
+}
